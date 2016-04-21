4ad9fa017735fc827db49e39910ecf63e0b97ab6
Dominic McCaffrey <dpmccaff5@gmail.com>
2016-04-21 13:39:40 -0500

    Converted data

---
+++ iand1cm.xyz_converted/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 1762428,
+    "boundingBox": {
+        "lx": 7.956802,
+        "ly": 94.929703,
+        "lz": -0.994156,
+        "ux": 14.10112,
+        "uy": 101.074021,
+        "uz": 5.150162
+    },
+    "tightBoundingBox": {
+        "lx": 7.956802,
+        "ly": 94.929703,
+        "lz": -0.994156,
+        "ux": 14.10112,
+        "uy": 99.119308,
+        "uz": -0.609848
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.05321135371923447,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
